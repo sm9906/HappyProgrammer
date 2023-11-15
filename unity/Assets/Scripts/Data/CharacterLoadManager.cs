329182ef6a21b861e1f072c8d50b0ee4594b0a1f
정진욱 <wlsdnr214@naver.com>
2023-11-15 13:38:58 +0900

    Feat: UI, Quest manager 구현

--- unity/Assets/Scripts/Data/CharacterLoadManager.cs
+++ unity/Assets/Scripts/Data/CharacterLoadManager.cs
@@ -1,47 +1,17 @@
 using System.Collections;
-using System.Text;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
 
-public class CharacterSaveLoadManager : MonoBehaviour {
+public class CharacterLoadManager : MonoBehaviour {
 
     string serverUrl = "http://k9d210.p.ssafy.io:8081/api/character";
     public GameObject checkWindow;
     public TextMeshProUGUI windowText;
     public GameObject uiManagerPrefab;
+    public GameObject questManagerPrefab;
     string accessToken;
-
-    // 캐릭터 게임 서버에 저장
-    public IEnumerator SaveCharacterData()
-    {
-        // 캐릭터 데이터를 JSON으로 변환
-        string json = JsonUtility.ToJson(DataManager.instance.CharacterData);
-
-        // POST 요청 보내기
-        using (UnityWebRequest www = new UnityWebRequest(serverUrl + "/save", "POST"))
-        {
-            byte[] bodyRaw = Encoding.UTF8.GetBytes(json);
-            www.uploadHandler = new UploadHandlerRaw(bodyRaw);
-            www.downloadHandler = new DownloadHandlerBuffer();
-            www.SetRequestHeader("Content-Type", "application/json");
-
-            yield return www.SendWebRequest();
-
-            if (www.result != UnityWebRequest.Result.Success)
-            {
-                Debug.Log(www.error);
-                windowText.text = "저장에 실패하였습니다.";
-                checkWindow.SetActive(true);
-            }
-            else
-            {
-                windowText.text = "캐릭터 정보 저장이 성공하였습니다.";
-                checkWindow.SetActive(true);
-            }
-        }
-    }
 
     // 캐릭터 서버에서 불러오기
     public IEnumerator LoadCharacterData()
@@ -74,6 +44,11 @@ public class CharacterSaveLoadManager : MonoBehaviour {
                 if (UIManager.instance == null)
                 {
                     Instantiate(uiManagerPrefab);
+                }
+                // QuestManager 인스턴스 생성
+                if (QuestManager.instance == null)
+                {
+                    Instantiate(questManagerPrefab);
                 }
                 SceneManager.LoadScene(DataManager.instance.CharacterData.savepoint);
                 Debug.Log("캐릭터 불러오기 성공");
